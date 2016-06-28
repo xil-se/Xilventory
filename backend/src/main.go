4a2f86a7d6856d3c8f7c7caef16e60d76225c393
Simon Vikstrom <sv@one.com>
2016-06-28 15:48:42 +0200

    Make it nice in tidy the google way

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,63 +1,61 @@
 package main
 
 import (
-	"v1api"
-	_ "github.com/lib/pq"
+	"fmt"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 	"log"
 	"net/http"
 	"strings"
-	"fmt"
+	"v1api"
 )
 
 var db *sqlx.DB
 
-
 func initializeDatabase(url string) *sqlx.DB {
-        var err error
-        db, err = sqlx.Open("postgres", url)
-        if err != nil {
-                log.Fatalf("Could not create database connection pool: %s", err)
-                return nil
-        }
-        err = db.Ping()
-        if err != nil {
-                log.Printf("Warning! Could not open database connection: %s", err)
-                return nil
-        }
+	var err error
+	db, err = sqlx.Open("postgres", url)
+	if err != nil {
+		log.Fatalf("Could not create database connection pool: %s", err)
+		return nil
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Printf("Warning! Could not open database connection: %s", err)
+		return nil
+	}
 
-        db.SetMaxIdleConns(5)
-        db.SetMaxOpenConns(50)
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(50)
 
-		return db
+	return db
 }
 
-func enforceContentType(c *web.C, h http.Handler) http.Handler{
+func enforceContentType(c *web.C, h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		contentType := r.Header.Get("Content-Type")
 		w.Header().Set("Content-Type", "application/vnd.api+json")
 
-		if ! strings.HasPrefix(contentType, "application/vnd.api+json") {
+		if !strings.HasPrefix(contentType, "application/vnd.api+json") {
 			w.WriteHeader(http.StatusUnsupportedMediaType)
 			w.Write([]byte("{error: \"Unsupported mediatype\"}"))
 			return
 		}
 
-
-		h.ServeHTTP(w,r)
+		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
 }
 
-func DBContext(c *web.C, h http.Handler) http.Handler{
+func DBContext(c *web.C, h http.Handler) http.Handler {
 	db := initializeDatabase("user=xilventory dbname=schematest password=123456")
 	fmt.Printf("running")
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		c.Env["db"] = db
-		h.ServeHTTP(w,r)
+		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
 }
--- backend/src/v1api/main.go
+++ backend/src/v1api/main.go
@@ -1,28 +1,25 @@
 package v1api
 
 import (
-        "fmt"
-        _ "github.com/lib/pq"
-        "net/http"
-        "github.com/zenazn/goji/web"
-        _ "github.com/lib/pq"
+	"fmt"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/zenazn/goji/web"
+	"net/http"
 )
 
-
 type Now struct {
-        Now string `db:"now"`
+	Now string `db:"now"`
 }
 
 func Hello(context web.C, w http.ResponseWriter, r *http.Request) {
 
-        now := []Now{}
+	now := []Now{}
 
-        context.Env["db"].(*sqlx.DB).Select(&now, "SELECT now()")
-        fmt.Fprintf(w, "%v", now[0])
+	context.Env["db"].(*sqlx.DB).Select(&now, "SELECT now()")
+	fmt.Fprintf(w, "%v", now[0])
 
-        fmt.Fprintf(w, "Hello, %s!", context.URLParams["name"])
-        fmt.Fprintf(w, "Hello, %v!", now)
-        
-        
+	fmt.Fprintf(w, "Hello, %s!", context.URLParams["name"])
+	fmt.Fprintf(w, "Hello, %v!", now)
+
 }
